2a2ec06e21b1a05518bc0cc2ce17b7a2e890ae78
Anshul Singh Chauhan <anshulchauhanworks@gmail.com>
2025-09-19 00:27:03 +0530

    refresh

--- src/pages/Hub.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Hub = () => {
-  return (
-    <div>Hub</div>
-  )
-}
-
-export default Hub
--- src/pages/TripPlan.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const TripPlan = () => {
-  return (
-    <div>TripPlan</div>
-  )
-}
-
-export default TripPlan
