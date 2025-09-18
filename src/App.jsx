d83714a4a05a56c8fd288e0aad688dede816b72a
Anshul Singh Chauhan <anshulchauhanworks@gmail.com>
2025-09-19 04:36:00 +0530

    routes added and login corrected

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,6 @@
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
-import Login from "./components/Login";
-import Register from "./components/Register";
-import Dashboard from "./components/Dashboard";
-import BusChatbot from "./components/Chatbot.jsx";
-import Ticket from "./components/Ticket";
+import React from "react";
+import AppRoutes from "./route/route";
 
 export default function App() {
-  return (
-    <Router>
-      <Routes>
-        {/* Default redirect to login */}
-        <Route path="/" element={<Navigate to="/login" replace />} />
-
-        {/* Auth screens */}
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/chatbot" element={<BusChatbot />} />
-        <Route path="/ticket" element={<Ticket />} />
-      </Routes>
-    </Router>
-  );
+  return <AppRoutes />;
 }
